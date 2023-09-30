d5d3c43d04e4c4b4f831032412a0794c6e65fdfe
adber94 <adber1503@gmail.com>
2023-09-29 21:57:16 -0500

    se hacen ajustes para que permita editar cantidades de stock.quant

--- fermotex/models/stock_quant.py
+++ fermotex/models/stock_quant.py
@@ -8,12 +8,6 @@ class StockQuantInherith(models.Model):
     quantity_fermo = fields.Float('Quantity', store=True)
 
     @api.model
-    def _get_inventory_fields_create(self):
-        """ Returns a list of fields user can edit when he want to create a quant in `inventory_mode`.
-        """
-        return ['product_id', 'location_id', 'lot_id', 'package_id', 'owner_id'] + self._get_inventory_fields_write()
-
-    @api.model
     def _get_inventory_fields_write(self):
         """ Returns a list of fields user can edit when he want to edit a quant in `inventory_mode`.
         """
@@ -62,3 +56,47 @@ class StockQuantInherith(models.Model):
             }
         self._apply_inventory()
         self.inventory_quantity_set = False
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """ Override to handle the "inventory mode" and create a quant as
+        superuser the conditions are met.
+        """
+        quants = self.env['stock.quant']
+        is_inventory_mode = self._is_inventory_mode()
+        # allowed_fields = self._get_inventory_fields_create()
+        for vals in vals_list:
+            if is_inventory_mode and any(f in vals for f in ['inventory_quantity', 'inventory_quantity_auto_apply']):
+                # if any(field for field in vals.keys() if field not in allowed_fields):
+                #     raise UserError(_("Quant's creation is restricted, you can't do this operation."))
+                auto_apply = 'inventory_quantity_auto_apply' in vals
+                inventory_quantity = vals.pop('inventory_quantity_auto_apply', False) or vals.pop(
+                    'inventory_quantity', False) or 0
+                # Create an empty quant or write on a similar one.
+                product = self.env['product.product'].browse(vals['product_id'])
+                location = self.env['stock.location'].browse(vals['location_id'])
+                lot_id = self.env['stock.lot'].browse(vals.get('lot_id'))
+                package_id = self.env['stock.quant.package'].browse(vals.get('package_id'))
+                owner_id = self.env['res.partner'].browse(vals.get('owner_id'))
+                quant = self._gather(product, location, lot_id=lot_id, package_id=package_id, owner_id=owner_id,
+                                     strict=True)
+                if lot_id:
+                    quant = quant.filtered(lambda q: q.lot_id)
+                if quant:
+                    quant = quant[0].sudo()
+                else:
+                    quant = self.sudo().create(vals)
+                if auto_apply:
+                    quant.write({'inventory_quantity_auto_apply': inventory_quantity})
+                else:
+                    # Set the `inventory_quantity` field to create the necessary move.
+                    quant.inventory_quantity = inventory_quantity
+                    quant.user_id = vals.get('user_id', self.env.user.id)
+                    quant.inventory_date = fields.Date.today()
+                quants |= quant
+            else:
+                quant = super().create(vals)
+                quants |= quant
+                if self._is_inventory_mode():
+                    quant._check_company()
+        return quants
